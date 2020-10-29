47109ff03b346a259385a9849144900709886f4d
Helmut Seidl <helmut.seidl@tum.de>
2020-10-29 15:25:14 +0100

    Exercise-Template pushed by Artemis

---
+++ src/fop/w5dll/BubbleSort.java
@@ -0,0 +1,11 @@
+package fop.w5dll;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+}
---
+++ src/fop/w5dll/MergeSort.java
@@ -0,0 +1,12 @@
+package fop.w5dll;
+
+import java.util.*;
+
+public class MergeSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
