6a542cb2ba5f322f5801f24f01971f33e323beff
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-12 02:40:46 +0400

    done

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -1,33 +1,108 @@
 package fop.w5dll;
 
 public class IntDoubleList {
-	private IntDoubleListElement head, tail;
+    private IntDoubleListElement head, tail;
 
-	public IntDoubleList() {
-		head = null;
-		tail = null;
-	}
+    public IntDoubleList() {
+        head = null;
+        tail = null;
+    }
 
-	public IntDoubleListElement getFirstElement(){
-		return head;
-	}
+    public IntDoubleListElement getFirstElement() {
+        return head;
+    }
 
 
-	public IntDoubleListElement getLastElement(){
-		return tail;
-	}
+    public IntDoubleListElement getLastElement() {
+        return tail;
+    }
 
-	public void append(int info){
-		IntDoubleListElement element = new IntDoubleListElement(info);
-		if (tail != null) {
-			tail.next = element;
-			element.prev = tail;
-			tail = element;
-			element.next = null;
-		}
-		else {
-			tail = element;
-			head = element;
-		}
-	}
+    public void append(int info) {
+        IntDoubleListElement element = new IntDoubleListElement(info);
+        if (tail != null) {
+            tail.next = element;
+            element.prev = tail;
+            tail = element;
+            element.next = null;
+        } else {
+            tail = element;
+            head = element;
+        }
+    }
+
+    public int size(){
+        IntDoubleListElement current = head;
+        int counter = 0;
+        while(current != null){
+            counter++;
+            current = current.next;
+        }
+        return counter;
+    }
+
+    public int get(int pos){
+        int counter = 0;
+        IntDoubleListElement current = head;
+        if (size() <= pos || pos < 0){
+            System.out.println("Position out of bounds");
+            return 0;
+        }
+        if (pos == 0) return current.getInfo();
+        while (counter != pos){
+            counter++;
+            current = current.next;
+        }
+        return current.getInfo();
+    }
+
+    public void remove(int pos){
+        IntDoubleListElement current = head;
+        int counter = 0;
+        if (current == null){
+            System.out.println("List empty");
+            return;
+        }
+        if (size() <= pos || pos < 0){
+            System.out.println("Position out of bounds");
+            return;
+        }
+        if (counter != pos){
+            counter++;
+            current = current.next;
+        }
+        if (counter == pos){
+            if (current == head && current == tail){
+                head = null;
+                tail = null;
+            }
+            if (current == head && current.next == tail){
+                head = tail;
+                tail.prev = null;
+            }
+            if (current == tail && current.prev == head){
+                tail = head;
+                head.next = null;
+            }
+            if (current.prev != null && current.next != null){
+
+            }
+        }
+    }
+
+    public String toString() {
+        IntDoubleListElement current = head;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (current != null){
+            if (current == tail){
+                stringBuilder.append(current.getInfo());
+            }
+            if (current.next != null){
+                stringBuilder.append(current.getInfo()).append(", ");
+                current = current.next;
+            }
+            else current = null;
+        }
+        return stringBuilder.toString();
+    }
 }
