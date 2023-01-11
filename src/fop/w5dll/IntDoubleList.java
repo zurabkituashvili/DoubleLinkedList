e7c24e14c1d299e0cc528fac9886733deb796900
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-12 00:14:18 +0400

    done

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -4,6 +4,11 @@ public class IntDoubleList {
 	private IntDoubleListElement head, tail;
 	private IntDoubleListElement[] listElements;
 
+	public IntDoubleList(IntDoubleListElement head, IntDoubleListElement tail, IntDoubleListElement[] listElements) {
+		this.head = head;
+		this.tail = tail;
+		this.listElements = listElements;
+	}
 
 	public IntDoubleListElement getFirstElement(){
 		return this.head;
