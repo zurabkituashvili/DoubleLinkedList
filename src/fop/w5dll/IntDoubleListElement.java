9b46534e75c4894d3abcdc7b92e7ad39c7c4d9b4
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-12 00:13:41 +0400

    done

--- src/fop/w5dll/IntDoubleListElement.java
+++ src/fop/w5dll/IntDoubleListElement.java
@@ -1,12 +1,20 @@
 package fop.w5dll;
 
 public class IntDoubleListElement {
-    private int[] info;
+    private int info;
     public IntDoubleListElement next, prev;
 
-    public IntDoubleListElement(int info, IntDoubleListElement next, IntDoubleListElement prev) {
-        this.info = new int[]{info} ;
-        this.next = next;
-        this.prev = prev;
+    public IntDoubleListElement(int info) {
+        this.info = info;
+        next = null;
+        prev = null;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
     }
 }
