41c64d17acb97f979019cbc589cbf573e90a7040
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-12 04:31:41 +0400

    done

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -127,15 +127,10 @@ public class IntDoubleList {
     public int sum(){
         int sum = 0;
         IntDoubleListElement curr = head;
-        if (curr != null) {
-            if (curr == tail) {
-                return curr.getInfo();
-            }
-            while (curr != tail) {
+        while (curr != null) {
                 sum += curr.getInfo();
                 curr = curr.next;
             }
-        }
         return sum;
     }
 
