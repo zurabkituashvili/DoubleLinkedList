285ded67aac2335900389cca4e212099e0e55803
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-15 00:49:33 +0400

    done

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -33,67 +33,67 @@ public class IntDoubleList {
         }
     }
 
-    public int size(){
+    public int size() {
         IntDoubleListElement current = head;
         int counter = 0;
-        while(current != null){
+        while (current != null) {
             counter++;
             current = current.next;
         }
         return counter;
     }
 
-    public int get(int pos){
+    public int get(int pos) {
         int counter = 0;
         IntDoubleListElement current = head;
-        if (size() <= pos || pos < 0){
+        if (size() <= pos || pos < 0) {
             System.out.println("Position out of bounds");
             return 0;
         }
         if (pos == 0) return current.getInfo();
-        while (counter != pos){
+        while (counter != pos) {
             counter++;
             current = current.next;
         }
         return current.getInfo();
     }
 
-    public void remove(int pos){
+    public void remove(int pos) {
         IntDoubleListElement current = head;
         int counter = 0;
-        if (current == null){
+        if (current == null) {
             System.out.println("List empty");
             return;
         }
-        if (size() <= pos || pos < 0){
+        if (size() <= pos || pos < 0) {
             System.out.println("Position out of bounds");
             return;
         }
-        while (counter != pos){
+        while (counter != pos) {
             counter++;
             current = current.next;
         }
-        if (current == head && current == tail){
+        if (current == head && current == tail) {
             head = null;
             tail = null;
         }
-        if (current == head && current.next == tail){
-            head = tail;
-            tail.prev = null;
-        }
-        if (current == tail && current.prev == head){
-            tail = head;
-            head.next = null;
-        }
-        if (current.prev != null && current.next == null){
+//        if (current == head && current.next == tail) {
+//            head = tail;
+//            tail.prev = null;
+//        }
+//        if (current == tail && current.prev == head) {
+//            tail = head;
+//            head.next = null;
+//        }
+        if (current.prev != null && current.next == null) {
             tail = current.prev;
             tail.next = null;
         }
-        if (current.next != null && current.prev == null){
+        if (current.next != null && current.prev == null) {
             head = current.next;
             head.prev = null;
         }
-        if (current.prev != null && current.next != null){
+        if (current.prev != null && current.next != null) {
             (current.next).prev = current.prev;
             (current.prev).next = current.next;
         }
@@ -103,27 +103,26 @@ public class IntDoubleList {
         IntDoubleListElement current = head;
         StringBuilder stringBuilder = new StringBuilder();
 
-        while (current != null){
-            if (current == tail){
+        while (current != null) {
+            if (current == tail) {
                 stringBuilder.append(current.getInfo());
             }
-            if (current.next != null){
+            if (current.next != null) {
                 stringBuilder.append(current.getInfo()).append(",");
                 current = current.next;
-            }
-            else current = null;
+            } else current = null;
         }
         return stringBuilder.toString();
     }
 
-    public boolean isEqual(IntDoubleList other){
-        if (other == null){
+    public boolean isEqual(IntDoubleList other) {
+        if (other == null) {
             return false;
         }
         if (size() != other.size()) return false;
         IntDoubleListElement curr = head;
         IntDoubleListElement otherCurr = other.head;
-        while (curr != null){
+        while (curr != null) {
             if (!curr.isEqual(otherCurr)) {
                 return false;
             }
@@ -133,35 +132,34 @@ public class IntDoubleList {
         return true;
     }
 
-    public int sum(){
+    public int sum() {
         int sum = 0;
         IntDoubleListElement curr = head;
         while (curr != null) {
-                sum += curr.getInfo();
-                curr = curr.next;
-            }
+            sum += curr.getInfo();
+            curr = curr.next;
+        }
         return sum;
     }
 
-    public IntDoubleList copy(){
+    public IntDoubleList copy() {
         IntDoubleList copy = new IntDoubleList();
         IntDoubleListElement curr = head;
-        for (int i = 0; i < size(); i++){
+        for (int i = 0; i < size(); i++) {
             copy.append(curr.getInfo());
             curr = curr.next;
         }
         return copy;
     }
 
-    public IntDoubleListElement[] search(int intValue){
+    public IntDoubleListElement[] search(int intValue) {
         IntDoubleListElement[] list = new IntDoubleListElement[size()];
         IntDoubleListElement curr = head;
-        for (int i = 0; i < size(); i++){
-            if (curr.getInfo() == intValue){
+        for (int i = 0; i < size(); i++) {
+            if (curr.getInfo() == intValue) {
                 list[i] = curr;
-                curr = curr.next;
             }
-            else curr = curr.next;
+            curr = curr.next;
         }
         return Arrays.stream(list).filter(Objects::nonNull).toArray(IntDoubleListElement[]::new);
     }
